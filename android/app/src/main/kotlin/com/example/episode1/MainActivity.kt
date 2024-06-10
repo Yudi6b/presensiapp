f6ba788da8cd3d9a006042a46f26f836e083d2a9
Yudi6b <over00845@gmail.com>
2024-06-10 15:06:27 +0700

    first upload

---
+++ android/app/src/main/kotlin/com/example/episode1/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.episode1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:episode1/screen/applo.dart';
+import 'package:episode1/screen/secoud.dart';
+import 'package:episode1/screen/login_dashboard.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title:'Presensi App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const SplashScreen(),
+    );
+  }
+}
